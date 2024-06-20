336dcedc0c03e10c4c079117a683082a3163c7bc
Huy Nguyen <nglehoanghuy066@gmail.com>
2024-06-20 22:33:59 +0700

    refactor: auth

--- server.py
+++ server.py
@@ -84,7 +84,7 @@ def login():
 
 @app.route("/auth/register", methods=["POST"])
 @cross_origin()
-def create_user():
+def register():
     try:
         data = request.json
         if not data:
@@ -131,136 +131,23 @@ def get_current_user():
         "data": user
     }), 200
 
-# @app.route("/users/", methods=["POST"])
-# def add_user():
-#     try:
-#         user = request.json
-#         if not user:
-#             return {
-#                 "message": "Please provide user details",
-#                 "data": None,
-#                 "error": "Bad request"
-#             }, 400
-#         is_validated = validate_user(**user)
-#         if is_validated is not True:
-#             return dict(message='Invalid data', data=None, error=is_validated), 400
-#         user = User().create(**user)
-#         if not user:
-#             return {
-#                 "message": "User already exists",
-#                 "error": "Conflict",
-#                 "data": None
-#             }, 409
-#         return {
-#             "message": "Successfully created new user",
-#             "data": user
-#         }, 201
-#     except Exception as e:
-#         return {
-#             "message": "Something went wrong",
-#             "error": str(e),
-#             "data": None
-#         }, 500
 
-
-# @app.route("/users/login", methods=["POST"])
-# def login():
-#     try:
-#         data = request.json
-#         if not data:
-#             return {
-#                 "message": "Please provide user details",
-#                 "data": None,
-#                 "error": "Bad request"
-#             }, 400
-#         # validate input
-#         is_validated = validate_email_and_password(
-#             data.get('email'), data.get('password'))
-#         if is_validated is not True:
-#             return dict(message='Invalid data', data=None, error=is_validated), 400
-#         user = User().login(
-#             data["email"],
-#             data["password"]
-#         )
-#         if user:
-#             try:
-#                 # token should expire after 24 hrs
-#                 user["token"] = jwt.encode(
-#                     {"user_id": user["_id"]},
-#                     app.config["SECRET_KEY"],
-#                     algorithm="HS256"
-#                 )
-#                 return {
-#                     "message": "Successfully fetched auth token",
-#                     "data": user
-#                 }
-#             except Exception as e:
-#                 return {
-#                     "error": "Something went wrong",
-#                     "message": str(e)
-#                 }, 500
-#         return {
-#             "message": "Error fetching auth token!, invalid email or password",
-#             "data": None,
-#             "error": "Unauthorized"
-#         }, 404
-#     except Exception as e:
-#         return {
-#             "message": "Something went wrong!",
-#             "error": str(e),
-#             "data": None
-#         }, 500
-
-
-# @app.route("/users/", methods=["GET"])
-# @token_required
-# def get_current_user(current_user):
-#     return jsonify({
-#         "message": "successfully retrieved user profile",
-#         "data": current_user
-#     })
-
-
-# @app.route("/users/", methods=["PUT"])
-# @token_required
-# def update_user(current_user):
-#     try:
-#         user = request.json
-#         if user.get("name"):
-#             user = User().update(current_user["_id"], user["name"])
-#             return jsonify({
-#                 "message": "successfully updated account",
-#                 "data": user
-#             }), 201
-#         return {
-#             "message": "Invalid data, you can only update your account name!",
-#             "data": None,
-#             "error": "Bad Request"
-#         }, 400
-#     except Exception as e:
-#         return jsonify({
-#             "message": "failed to update account",
-#             "error": str(e),
-#             "data": None
-#         }), 400
-
-
-# @app.route("/users/", methods=["DELETE"])
-# @token_required
-# def disable_user(current_user):
-#     try:
-#         User().disable_account(current_user["_id"])
-#         return jsonify({
-#             "message": "successfully disabled acount",
-#             "data": None
-#         }), 204
-#     except Exception as e:
-#         return jsonify({
-#             "message": "failed to disable account",
-#             "error": str(e),
-#             "data": None
-#         }), 400
-
+@app.route("/users", methods=["GET"])
+@cross_origin()
+@jwt_required()
+def gat_all_users():
+    try:
+        users = User(conn).get_all_users()
+        return {
+            "message": "Successfully gat all users",
+            "data": users
+        }, 201
+    except Exception as e:
+        return {
+            "message": "Something went wrong",
+            "error": str(e),
+            "data": None
+        }, 500
 
 # @app.route("/books/", methods=["POST"])
 # @token_required
@@ -399,22 +286,22 @@ def get_current_user():
 #         }), 400
 
 
-# @app.errorhandler(403)
-# def forbidden(e):
-#     return jsonify({
-#         "message": "Forbidden",
-#         "error": str(e),
-#         "data": None
-#     }), 403
+@app.errorhandler(403)
+def for_bidden(e):
+    return jsonify({
+        "message": "For bidden",
+        "error": str(e),
+        "data": None
+    }), 403
 
 
-# @app.errorhandler(404)
-# def forbidden(e):
-#     return jsonify({
-#         "message": "Endpoint Not Found",
-#         "error": str(e),
-#         "data": None
-#     }), 404
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({
+        "message": "Not found",
+        "error": str(e),
+        "data": None
+    }), 404
 
 
 if __name__ == "__main__":
